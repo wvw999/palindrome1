4ca9fe5667e96c8f1814d04d3200f9dedc47e78b
Eric Shull and Will Swanson <eric@>
2018-06-27 14:54:12 -0700

    second positive fail

--- spec/palindrome_spec.rb
+++ spec/palindrome_spec.rb
@@ -3,7 +3,12 @@ require('palindrome')
 
 describe('#palindrome') do
   it('receive input of text') do
-    palindrome = Word.new("cat")
+    palindrome = Word.new("tacocat")
     expect(palindrome.identify?()).to(eq(true))
   end
+  it('is input a palindrome') do
+    palindrome = Word.new("cat")
+    expect(palindrome.identify?()).to(eq(false))
+  end
+
   end
