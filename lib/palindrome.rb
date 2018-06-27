c3c3ee07c76a4d8d687da4a1c95b49235723e18f
Eric Shull and Will Swanson <will@>
2018-06-27 15:41:08 -0700

    work with any word now

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -4,8 +4,17 @@ class Word
   end
 
   def identify?()
-    if @palindrome
-      true
+    word_array = @palindrome.split("")
+    word_array_clone = word_array.clone
+    reversed_array = []
+    word_length = word_array.length
+     (1..word_length).each do |y|
+       reversed_array.push(word_array_clone.pop)
+     end
+    if word_array.join("") == reversed_array.join("")
+      return "equal!"
+    else
+      return "not a palindrome"
     end
   end
 end
--- spec/palindrome_spec.rb
+++ spec/palindrome_spec.rb
@@ -4,11 +4,11 @@ require('palindrome')
 describe('#palindrome') do
   it('receive input of text') do
     palindrome = Word.new("tacocat")
-    expect(palindrome.identify?()).to(eq(true))
+    expect(palindrome.identify?()).to(eq("equal!"))
   end
   it('is input a palindrome') do
     palindrome = Word.new("cat")
-    expect(palindrome.identify?()).to(eq(false))
+    expect(palindrome.identify?()).to(eq("not a palindrome"))
   end
 
   end
